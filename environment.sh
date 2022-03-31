aea403dd988881a6818ca125d100d098c2ee6cf3
sanghoon <57610448+shleee47@users.noreply.github.com>
2022-03-31 19:18:24 -0400

    Update environment.sh

--- environment.sh
+++ environment.sh
@@ -1,14 +1,11 @@
-conda create -y -n gen python=3.7.7
-conda activate gen
+conda create -y -n wifi python=3.8
+conda activate wifi
 conda install scipy
 ​
 ####select according to your conda version####
 ####https://pytorch.org/####
 conda install pytorch torchvision torchaudio cudatoolkit=11.1 -c pytorch -c nvidia
-#conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch
 ​
-conda install -c conda-forge librosa
 pip install PyYAML
 conda install -c conda-forge tqdm
 pip install tensorboard
-pip install torchlibrosa
